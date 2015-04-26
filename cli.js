1c62a07eed17bf9e2a19b8cb674f44cbf3c50e78
Touka <i@touka.me>
2015-04-26 15:14:03 +0800

    update cli

--- cli.js
+++ cli.js
@@ -1,4 +1,5 @@
 var fs = require('fs'),
+    path = require('path'),
     marked = require('marked'),
     yaml = require('js-yaml'),
     mkdirp = require('mkdirp'),
@@ -12,11 +13,18 @@ var dir = __dirname + '/_posts/',
 mkdirp.sync(api_posts)
 var config = yaml.load(fs.readFileSync('config.yaml', encoding))
 
-fs.readdir(dir, function (err, files) {
-  var ord = 0
-  files.forEach(function(file) {
+fs.readdir(dir, function (err, temp_files) {
+  var files = []
+  for(var i in temp_files) {
+    var ext = path.extname(temp_files[i])
+    if(ext == '.md' || ext == '.markdown'){
+      files.push(temp_files[i])
+    }
+  }
+  files.forEach(function(file, ord) {
     var file_content = fs.readFileSync(dir + file, encoding)
-    var meta = yaml.load(file_content.split('---')[0])
+    var meta = yaml.load(file_content.split('---')[1])
+    console.log('...' + meta.title)
     index.posts.push({
       ord: ord,
       title: meta.title
@@ -24,11 +32,11 @@ fs.readdir(dir, function (err, files) {
     var post = {
       ord: ord,
       title: meta.title,
-      content: marked(file_content.split('---')[1])
+      content: marked(file_content.split('---')[2])
     }
     post = JSON.stringify(post, null, 4)
     fs.writeFileSync(api_posts + ord + '.json', post, encoding)
-    if(files.length = ord + 1){
+    if(files.length == ord + 1){
       tpl = swig.compileFile(__dirname + '/' + config.theme + '.swig')
       var date = new Date
       config.build = date.getTime()
@@ -38,6 +46,5 @@ fs.readdir(dir, function (err, files) {
       fs.writeFileSync(__dirname + '/index.html', tpl(index), encoding)
       return
     }
-    ord++
   })
 })
